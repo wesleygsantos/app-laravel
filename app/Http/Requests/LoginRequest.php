1bc55f12027d7e5182c300ad0f21bfe081c78dab
wesleygsantos <wesleyg_santos@yahoo.com.br>
2024-07-31 22:59:43 -0300

    Desenvolvimento validação JWT

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        
+        $rules = [
+            'email' => [
+                'required',
+                'email:filter'
+            ],
+            'password' => [
+                'required',
+                'min:6',
+                'max:50',
+            ]
+        ];
+
+        return $rules;
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CidadeController;
 use App\Http\Controllers\Api\CidadesController;
 use App\Http\Controllers\Api\MarcaController;
 use App\Http\Controllers\Api\ProdutoController;
 use App\Http\Controllers\Api\UserController;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/users', UserController::class);
-Route::apiResource('/produtos', ProdutoController::class);
+Route::middleware('api')->apiResource('/produtos', ProdutoController::class);
 Route::apiResource('/cidades', CidadesController::class);
 Route::apiResource('/marcas', MarcaController::class);
 
 
-Route::get('/', function () {
+Route::middleware('api')->get('/', function () {
     return response()->json([
         'success' => true
     ]);
 });
+
+Route::post('auth/register', [AuthController::class, 'register']);
+Route::post('auth/login', [AuthController::class, 'login']);
